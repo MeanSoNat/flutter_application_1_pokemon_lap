cc697556467a6e6edf87776e13e72ba273c4ffb8
MeanSoNat <nattaphon_wo@kkumail.com>
2023-07-15 21:35:42 +0700

    e

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # USE STARWAR APIs
+## Sources
 - API: https://swapi.dev/api/people
 - jsonModel: https://app.quicktype.io/
+
+## Install dependencies
+- pub 
--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -51,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'dsdsd',
             ),
             Text(
               '$_counter',
@@ -64,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
